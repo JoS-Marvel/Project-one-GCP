36ec491c1fae215ff401909bb7b83c5ef03d6b06
JoS-Marvel <85107914+JoS-Marvel@users.noreply.github.com>
2021-06-09 17:54:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req,res) => {
     console.log('new connection')
-    res.end('Hello This is first DevOps Pipelinne') 
+    res.end('Hello This is first DevOps Pippelinne') 
 })
 const PORT = process.env.port || 8080
 server.listen(PORT,() => console.log('Listening'))
