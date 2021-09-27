d7268c5c293341b268d6ed2b4b87f22b2fa8d3f6
JoS-Marvel <85107914+JoS-Marvel@users.noreply.github.com>
2021-09-27 11:14:54 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http')
 const server = http.createServer((req,res) => {
     console.log('new connection')
-    res.end('Hello This is second Devops12 -sample Pipeline') 
+    res.end('Hello This is second Devops1 -sample Pipeline') 
 })
 const PORT = process.env.port || 8080
 server.listen(PORT,() => console.log('Listening'))
